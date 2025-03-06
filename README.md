92740cdc741c664050b0e08fe3d7c5c033af7f7c
Levani migriauli <levani.migriauli@lb.ge>
2025-03-06 22:34:14 +0400

    update README to include repository purpose and enhance title with emoji

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Asynchronous JavaScript: Promises, Async/Await, and AJAX
+🚀 Asynchronous JavaScript: Promises, Async/Await, and AJAX
+This repository is for study purposes and contains practice exercises from a Udemy course on Asynchronous JavaScript,
+covering key concepts like Promises, Async/Await, and AJAX.
---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// COUNTRIES API URL
+// https://restcountries.com/v2/name/portugal
+
+// REVERSE GEOCODING API URL
+// https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
+
+///////////////////////////////////////
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
