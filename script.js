fa6d5fd15db9d2f5155e76efe03348bbed2153a9
Levani migriauli <levani.migriauli@lb.ge>
2025-03-12 17:20:41 +0400

    Throwing errors manually

--- script.js
+++ script.js
@@ -120,23 +120,30 @@ const renderError = function(msg) {
 //     })
 // };
 
+const getJSON = function(url, errorMsg = 'Something went wrong') {
+  return fetch(url)
+    .then(
+      response => {
+        if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+        return response.json();
+      });
+};
+
 const getCountryData = function(country) {
   // Country 1
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(
-      response => response.json())
+  getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
     .then(data => {
-      console.log(data);
       renderCountry(data[0]);
       const neighbour = data[0].borders[0];
-      if (!neighbour) return;
+
+      if (!neighbour) throw new Error('No neighbour found!');
 
       // Country 2
-      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+      return getJSON(`https://restcountries.com/v2/alpha/${neighbour}`, 'Country not found');
     })
-    .then(response => response.json())
     .then(data => renderCountry(data, 'neighbour'))
     .catch(err => {
+      console.log(err);
       console.error(`${err} 💥`);
       renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
     })
@@ -148,3 +155,5 @@ const getCountryData = function(country) {
 btn.addEventListener('click', function() {
   getCountryData('georgia');
 });
+
+getCountryData('georgia');
