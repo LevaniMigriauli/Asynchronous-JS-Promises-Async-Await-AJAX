5fbbde2d2e99a6d849a15cd63b40b6bd5e6b2d62
Levani migriauli <levani.migriauli@lb.ge>
2025-03-25 23:50:10 +0400

    building a simple promise

--- script.js
+++ script.js
@@ -194,3 +194,46 @@ btn.addEventListener('click', function() {
 //   console.log(res);
 // })
 // console.log('Test end');
+
+const lotteryPromise = new Promise(function(resolve, reject) {
+  console.log('Lottery draw is happening 🔮');
+  setTimeout(function() {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your monet 💩'));
+    }
+  }, 2000);
+});
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// Promisifying setTimeout
+const wait = function(seconds) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1).then(() => {
+  console.log('I waited for 1 second');
+  return wait(1);
+}).then(() => {
+  console.log('I waited for 2 seconds');
+  return wait(2);
+}).then(() => {
+  console.log('I waited for 3 seconds');
+  return wait(3);
+});
+// callback hell
+// setTimeout(() => {
+//   console.log('1 second passed');
+//   setTimeout(() => {
+//     console.log('2 seconds passed');
+//     setTimeout(() => {
+//       console.log('3 seconds passed');
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).catch(x => console.log(x));
